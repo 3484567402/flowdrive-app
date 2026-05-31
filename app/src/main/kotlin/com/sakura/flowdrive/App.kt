9c0a99e256692b3d05de060d67a7e61e2b9a8e57
3484567402 <3484567402@users.noreply.github.com>
2026-05-31 08:27:06 +0000

    feat: 移植Crash和日志模块
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ app/src/main/kotlin/com/sakura/flowdrive/App.kt
@@ -0,0 +1,19 @@
+package com.sakura.flowdrive
+
+import android.app.Application
+import com.sakura.flowdrive.core.util.AppSettings
+import com.sakura.flowdrive.core.util.CrashHandler
+import com.sakura.flowdrive.core.util.Logger
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AppSettings.init(this)
+        Logger.initialize(this)
+        CrashHandler.init(this)
+        CrashHandler.setCrashCallback { context, crashInfo ->
+            CrashActivity.start(context, crashInfo)
+        }
+    }
+}
