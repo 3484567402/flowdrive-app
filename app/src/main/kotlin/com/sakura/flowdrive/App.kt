8cbe12fcf528de7d324280b4d900251bfe00ed87
3484567402 <110465199+3484567402@users.noreply.github.com>
2026-05-31 17:27:57 +0800

    Last Sync: 2026-05-31 17:27 (Mobile)

---
+++ app/src/main/kotlin/com/sakura/flowdrive/App.kt
@@ -0,0 +1,17 @@
+package com.sakura.flowdrive
+
+import android.app.Application
+import com.sakura.flowdrive.core.util.AppSettings
+import com.sakura.flowdrive.core.util.CrashHandler
+import com.sakura.flowdrive.core.util.Logger
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppSettings.init(this)
+        Logger.initialize(this)
+        CrashHandler.init(this) { context, crashInfo ->
+            CrashActivity.start(context, crashInfo)
+        }
+    }
+}
