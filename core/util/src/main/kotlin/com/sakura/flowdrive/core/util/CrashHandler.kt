2409e8914f03b1226711f7572fcd522bc9781559
3484567402 <3484567402@users.noreply.github.com>
2026-06-01 07:52:50 +0000

    feat: 增加语言切换设置
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- core/util/src/main/kotlin/com/sakura/flowdrive/core/util/CrashHandler.kt
+++ core/util/src/main/kotlin/com/sakura/flowdrive/core/util/CrashHandler.kt
@@ -46,7 +46,7 @@ object CrashHandler {
         val errorLog = buildString {
             appendLine("[Crash Report]")
             appendLine("Time: $timeStr")
-            appendLine("Thread: ${thread.name} (ID: ${thread.id})")
+            appendLine("Thread: ${thread.name} (tid=${thread.threadId})")
             appendLine("Device: ${android.os.Build.MODEL} (API ${android.os.Build.VERSION.SDK_INT})")
             appendLine()
             appendLine("Exception: ${throwable.javaClass.simpleName}")
